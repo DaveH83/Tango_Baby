186e833252aa63314d07bbd8ea37ae315aa343fe
Daniel Erb <erb.daniel@outlook.com>
2023-05-01 20:21:57 -0400

    basic user model and home splash page

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class App_user (AbstractUser):
+    last_updated = models.DateField(auto_now=True)
+    REQUIRED_FIELDS = ["email"]
+    
+
+    def __str__(self):
+        return f"This is the class object for username: {self.username}"
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+# Serve the react app
+@api_view(["GET"])
+def home(request):
+    # Get the index for the react app
+    # file = open("static/index.html")
+    # Read it to a variable
+    # resp = file.read()
+    # So we can then responsibly close the file and serve the app
+    # file.close()
+    return HttpResponse("Django Home")
