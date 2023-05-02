2b07a22cbbbe124a2a538791bbc83193fa0ea6d5
cmstiver <chayse.stiver@gmail.com>
2023-05-02 08:01:33 -0700

    connected React to Django

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		// this is the folder where vite will generate its output. Make sure django can serve files from here!
+		outDir: "../static/",
+
+		// delete the old build when creating the new build.
+		// this is the default behavior, unless outDir is outside of the current directory
+		emptyOutDir: true,
+
+		// vite will generate sourcemaps, which let you see logs and error messages with line numbers from our jsx files, not from the minified js
+		sourcemap: true,
+	},
+	plugins: [react()],
+});
--- user_app/views.py
+++ user_app/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import authenticate, login, logout
 from .models import App_user
 
 
+def front(request):
+    index = open('static/index.html')
+    return HttpResponse(index)
+
+
 @api_view(["POST"])
 def register_user(request):
     # Handle creating new user, this will change in time as the user model grows for more profile information
@@ -30,7 +35,7 @@ def register_user(request):
     except Exception as e:
         print(e)
         return JsonResponse({"username": None})
-    
+
 
 @api_view(["POST"])
 def user_login(request):
@@ -58,15 +63,3 @@ def user_logout(request):
     except Exception as e:
         print(e)
         return JsonResponse({"success": False})
-
-
-# Serve the react app
-@api_view(["GET"])
-def home(request):
-    # Get the index for the react app
-    # file = open("static/index.html")
-    # Read it to a variable
-    # resp = file.read()
-    # So we can then responsibly close the file and serve the app
-    # file.close()
-    return HttpResponse("Django Home")
