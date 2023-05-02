f31f88ccd6f3ee42db663b7dbff34eb5cf44a2cd
Daniel Erb <erb.daniel@outlook.com>
2023-05-02 10:17:32 -0400

    create superuser for dan-dev

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import App_user
+from .models import App_user, Child, Name, Voted_Name
 
-admin.site.register(App_user, UserAdmin)
+admin.site.register(App_user)
+admin.site.register(Child)
+admin.site.register(Name)
+admin.site.register(Voted_Name)
