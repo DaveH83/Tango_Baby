0d3a3f2e4dca9b11b40538d287bfde4ae25d15ba
Daniel Erb <erb.daniel@outlook.com>
2023-05-03 10:21:44 -0400

    update views for naming

--- name_app/urls.py
+++ name_app/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 # All patterns proceeding by 'app/'
 urlpatterns = [
-    # path('', views.front),
     path("children/", views.handle_children),
     path("name/", views.handle_name),
-    path("names/", views.handle_names),
+    path("ranking/", views.handle_ranking),
 ]
