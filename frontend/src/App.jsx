010ffb7a02a987a3be689fa5e32c406fb37c2d74
cmstiver <chayse.stiver@gmail.com>
2023-05-03 09:00:12 -0700

    fixed layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import Auth from "./pages/Auth";
 
 export default function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		setIsLoggedIn(true);
+	}, []);
+
 	return (
 		<div>
 			<Header />
-			<div className="mt-16">
-				<Outlet />
-			</div>
+			<div className="mt-16 md:ml-64">{isLoggedIn ? <Outlet /> : <Auth />}</div>
 		</div>
 	);
 }
