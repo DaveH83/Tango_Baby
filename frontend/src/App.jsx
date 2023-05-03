abcffeb53ff280f1ad5ce15a05f7747b50337c6f
cmstiver <chayse.stiver@gmail.com>
2023-05-03 12:37:50 -0700

    created login/signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,54 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Auth from "./pages/Auth";
+import axios from "axios";
 
-export default function App() {
+axios.defaults.withCredentials = true;
+
+export const UserInfo = createContext({});
+
+export function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [userInfo, setUserInfo] = useState({});
+
+	async function getUserInfo() {
+		const response = await axios.get("/user/curr-user/");
+		if (response.data.error) {
+			return;
+		} else {
+			setUserInfo(response.data);
+			setIsLoggedIn(true);
+		}
+	}
 
 	useEffect(() => {
-		setIsLoggedIn(true);
+		getUserInfo();
 	}, []);
 
 	return (
-		<div>
-			<Header />
-			<div className="mt-16 md:ml-64">{isLoggedIn ? <Outlet /> : <Auth />}</div>
-		</div>
+		<UserInfo.Provider value={userInfo}>
+			<div>
+				{isLoggedIn ? (
+					<>
+						<Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+						<div className="mt-16 md:ml-64">
+							{isLoggedIn ? (
+								<Outlet />
+							) : (
+								<Auth setIsLoggedIn={setIsLoggedIn} setUserInfo={setUserInfo} />
+							)}
+						</div>
+					</>
+				) : (
+					<>
+						<Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+						<div className="mt-16">
+							<Auth setIsLoggedIn={setIsLoggedIn} setUserInfo={setUserInfo} />
+						</div>
+					</>
+				)}
+			</div>
+		</UserInfo.Provider>
 	);
 }
