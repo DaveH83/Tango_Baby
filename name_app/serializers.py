04aca983b278e436ae0e93ef6c1a3460d8b2d3ee
mickey <shoey25ms@gmail.com>
2023-05-08 03:39:30 +0200

    new viewsets

--- name_app/serializers.py
+++ name_app/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from ..user_app.models import *
 from rest_framework.validators import UniqueTogetherValidator
+from django.apps import apps
+App_User = apps.get_model('user_app', 'App_User')
+Child = apps.get_model('user_app', 'Child')
+Name = apps.get_model('user_app', 'Name')
+Voted_Name = apps.get_model('user_app', 'Voted_Name')
+Blacklist = apps.get_model('user_app', 'Blacklist')
 
 class NameSerializer(serializers.ModelSerializer):
 
