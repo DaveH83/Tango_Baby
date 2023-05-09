2dffe8bb145d18ab71d87093e731c496dffb68c7
Yen P <jinapham.0611@gmail.com>
2023-05-09 17:03:47 -0400

    fixing bug on swipe card for users with no children

--- frontend/src/pages/SwipeNames.jsx
+++ frontend/src/pages/SwipeNames.jsx
@@ -1,15 +1,17 @@
 import { useLoaderData, useParams } from "react-router-dom";
-import { useEffect,useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import TinderCard from 'react-tinder-card';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
 import { nameListLoader, swipeHandler } from '../Utilities/Utilities';
 import AddName from '../components/AddName';
+import { UserContext } from "../App";
 
 export default function SwipeNames() {
 	const nameList=useLoaderData();
 	const [toShowList,setToShowList]=useState(nameList.slice(0,2)); 	
 	const {uuid}=useParams();
+	const {children} = useContext(UserContext)
 
 	//handler for swiping name card
 	const onSwipe = (name, direction) => {
@@ -37,6 +39,7 @@ export default function SwipeNames() {
 			setToShowList((prevList) => prevList.slice(0, -1));
 			console.log(toShowList)
 	}}
+	//working on this, its not working yet
 	useEffect(async()=>{
 		if (toShowList.length===0){ 
 			console.log("noname to swipe")
@@ -49,13 +52,13 @@ export default function SwipeNames() {
         return (<div>Loading...</div>);
     }
 	return (
-		<div className="card-container">
+		children && ( 
+			<div className="card-container" >
 			<AddName />
 			{toShowList.map((name)=> (
 				<div className='single-card'>
 					<TinderCard className='tinder_card' onSwipe={(dir) => onSwipe(name, dir)} preventSwipe={['up', 'down']}>
 							<p className='name-holder' key={name.id}>{name.name}</p>
-
 					</TinderCard> 
 					<div className='icons'>
 						<button onClick={()=>onClick(name,'left')}>
@@ -64,14 +67,13 @@ export default function SwipeNames() {
 						<button onClick={()=>onClick(name, 'right')}>
 							<FavoriteIcon fontSize="large"/>
 						</button>
-						
 					</div>
 				</div>
-				
 			)	
 			)}
-			
 			</div>
+		)
+		
 			
 	
 	);
